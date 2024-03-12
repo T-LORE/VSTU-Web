0c719f0c2097c5ac1e0fd3a83cf761a5e6c5b6d1
Zverkov Maxim <tlore007@gmail.com>
2024-03-12 21:11:47 +0300

    feat(table): connected to database

--- LAB_2/test.php
+++ LAB_2/test.php
@@ -1,6 +1,25 @@
-<!doctype html>
-<html lang="en">
+<?php
+require_once 'dbconnection.php';
 
+$ip = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hairdressing_salon";
+
+$db = new Database($ip, $username, $password, $dbname );
+$connection = $db->getConnection();
+
+$sql = "SELECT services.*, halls.hall_number 
+        FROM services 
+        INNER JOIN halls ON services.id_hall = halls.id";
+        
+$result = $connection->query($sql);
+
+$services = $result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,7 +29,7 @@
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
+<body>
 
 <div class="container">
   <h2 class="my-4">Services</h2>
@@ -26,14 +45,16 @@
       </tr>
     </thead>
     <tbody>
+        <?php foreach ($services as $service): ?>
         <tr>
-          <td>Placeholder Id</td>
-          <td><img src="<?= "Assets/1.jpg" ?>" alt="Service Image" style="max-width: 200px;"></td>
-          <td>Placeholder name</td>
-          <td>Placeholder number</td>
-          <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, voluptate obcaecati. Vitae culpa beatae a autem quisquam magni modi eos deserunt commodi, optio alias sequi, aliquam soluta minus nostrum facere.</td>
-          <td>Placeholder cost руб.</td>
+            <td><?= $service["id"] ?></td>
+            <td><img src="<?= $service["image_path"] ?>" alt="Service Image" style="max-width: 200px;"></td>
+            <td><?= $service["name"] ?></td>
+            <td><?= $service["hall_number"] ?></td> <!-- Используем hall_number из halls -->
+            <td><?= $service["description"] ?></td>
+            <td><?= $service["cost"] ?> руб.</td>
         </tr>
+        <?php endforeach; ?>
     </tbody>
   </table>
 </div>
