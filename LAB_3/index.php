568b671f6a7f1577f3c8e1a0ce92d43a2d405361
Zverkov Maxim <tlore007@gmail.com>
2024-03-25 10:11:51 +0300

    refactor(): all components moved to folder

--- LAB_3/index.php
+++ LAB_3/index.php
@@ -17,11 +17,13 @@ require_once 'logic.php';
 <body>
     <!-- main container -->
     <div class="container-xxl px-4">   
-        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/header.php')?>
+        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/components/header.php')?>
+        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/components/registerform.php')?>
+        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/components/loginform.php')?>
         
-        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/table.php')?>
-        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/footer.php')?>
-        
+        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/components/table.php')?>
+        <?php require_once ($_SERVER['DOCUMENT_ROOT'] . '/LAB_3/components/footer.php')?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
